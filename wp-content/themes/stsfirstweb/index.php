fe2967659e04e2e2ebff219902a04d17d9074ceb
Satoshi <nirnirva.s.t.s1551@gmail.com>
2017-04-16 21:48:24 +0000

    add fonts and index.php for theme from scratch

--- wp-content/themes/stsfirstweb/index.php
+++ wp-content/themes/stsfirstweb/index.php
@@ -1,16 +1,19 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package stsfirstweb
- */
-
-get_header(); ?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+</head>
+<body>
+<div id="header">
+ <div id="headerimg">
+   <h1>
+    <a href="<?php echo get_option('home'); ?>">
+       <?php bloginfo('name'); ?></a>
+   </h1>
+     <div class="description">
+       <?php bloginfo('description'); ?>
+     </div>
+  </div>
+</div>
+</body>
+</html>
 
