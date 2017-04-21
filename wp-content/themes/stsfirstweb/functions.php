3324b9fc530153b3df4960e1442b34fc35e9fad3
Satoshi <nirnirva.s.t.s1551@gmail.com>
2017-04-21 11:42:05 +0000

    add admin menu test

--- wp-content/themes/stsfirstweb/functions.php
+++ wp-content/themes/stsfirstweb/functions.php
@@ -32,3 +32,25 @@ function sts_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'sts_scripts' );
 
+/** 
+ *  Test Admin menu 
+ */
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Step 1. */
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** Step 3. */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
+
